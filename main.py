69f017c3ffbe56f6353e233a957c9b2067018464
UJeans <jean207@naver.com>
2024-09-05 18:08:22 +0900

    [#3]feat: 예외적인 상황 위로

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ with open('config.json') as config_file:
 environment = 'development'  # 'production'으로 변경 시 배포용 URL 사용
 API_URL = config[environment]['API_URL']
 
-
 SWAGGER_HEADERS = {
     "title": "Code Syntax api",
     "version": "1.0.0",
@@ -45,19 +44,22 @@ def root():
 async def syntax_check(request_code: RequestCode):
     return_code, stdout = check_code(request_code.source_code)
 
-    if return_code == 0:
-        # 시각화 분석 엔진에게 분석 요청
-        response = requests.post(
-            API_URL,
-            json={"source_code": request_code.source_code}
-        )
-        return JSONResponse(
-            status_code=200,
-            content=response.json()
-        )
-    else:   # 문법 오류가 있을 때
+    # 문법 오류가 있을 때
+    if return_code != 0:
+        # 오류 내용과 함께 BAD_REQUEST 반환
         return JSONResponse(
             status_code=400,
             content={"error": extract_error_message(stdout)}
         )
+
+    # 시각화 분석 엔진에게 코드 분석 요청
+    response = requests.post(
+        API_URL,
+        json={"source_code": request_code.source_code}
+    )
+    return JSONResponse(
+        status_code=200,
+        content=response.json()
+    )
+
 
