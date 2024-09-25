cc2b9705ea09de09523869da5b7e2948d3e8131d
UJeans <jean207@naver.com>
2024-09-25 15:16:51 +0900

    [#5]fix: [] -> {} 변경

--- app/exception/base_custom_exception.py
+++ app/exception/base_custom_exception.py
@@ -5,4 +5,4 @@ class BaseCustomException(Exception):
     def __init__(self, status_code: int, custom_error: CustomError, result: dict):
         self.status_code = status_code
         self.custom_error = custom_error
-        self.result = [] if result is None else result
+        self.result = {} if result is None else result
--- app/exception/invalid_exception.py
+++ app/exception/invalid_exception.py
@@ -9,7 +9,7 @@ class InvalidException(BaseCustomException):
         super().__init__(
             status_code=status.HTTP_400_BAD_REQUEST,
             custom_error=custom_error,
-            result=[] if result is None else result
+            result={} if result is None else result
         )
 
 
