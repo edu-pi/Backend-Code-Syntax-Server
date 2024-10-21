7cbac4171a9d3c29f61a1325ad41d413fa51fcef
UJeans <jean207@naver.com>
2024-10-21 15:26:10 +0900

    [#23]fix: 리퀘스트 버그 수정

--- app/web/logger.py
+++ app/web/logger.py
@@ -19,13 +19,14 @@ def get_logger():
 
 
 # Request 로깅 미들웨어 - request 로깅 필요할 시 주석 제거
-# async def log_request(request: Request, call_next):
-#     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-#     body = await request.body()
-#     logger.info(f"[{current_time}] Request: {request.method} {request.url}  {body.decode()}")
-#
-#     response = await call_next(request)
-#     return response
+async def log_request(request: Request, call_next):
+    pass
+    # current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    # body = await request.body()
+    # logger.info(f"[{current_time}] Request: {request.method} {request.url}  {body.decode()}")
+    #
+    # response = await call_next(request)
+    # return response
 
 
 # Response 로깅 미들웨어
