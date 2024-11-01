5c7398dcd87b6685aaac4ca6b7b7d5cf98cbdca7
UJeans <jean207@naver.com>
2024-11-01 15:34:09 +0900

    [#29]chore: 테스트 추가

--- tests/route/execute/test_execute_service.py
+++ tests/route/execute/test_execute_service.py
@@ -1,7 +1,7 @@
-from app.route.execute.service.execute_service import _contains_forbidden_imports
+from app.route.execute.service import execute_service
 
 
 def test__contains_forbidden_imports():
     code = "import os\n"
 
-    assert _contains_forbidden_imports(code) is True
+    assert execute_service._contains_forbidden_imports(code) is True
